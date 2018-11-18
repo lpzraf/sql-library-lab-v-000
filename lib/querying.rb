b8a74eea02e5860a53c72427e9a6b73dac301c6a
Rafael Lopez <lpz.raf@gmail.com>
2018-11-18 21:26:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,8 +31,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
    FROM series 
-   INNER JOIN authors ON authors.id = author_id
-   INNER JOIN subgenres ON subgenre_id = subgenres.id"
+   ORDER BY LENGTH(series) DESC 
+   LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
