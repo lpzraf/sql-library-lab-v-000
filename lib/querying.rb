f0bff9c35412016b4fae3aec50d6e50ec0c763c1
Rafael Lopez <lpz.raf@gmail.com>
2018-11-18 21:19:39 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(characters.species) 
    FROM characters 
-   WHERE species > COUNT(characters.species)"
+   GROUP BY species HAVING COUNT(characters.species) >= 0;"
 end
 
 def select_name_and_series_subgenres_of_authors
